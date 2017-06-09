f5567fbb2f003a428655bcb638ade2c27f53e66d
Andy Chen <shinningstar1001@gmail.com>
2017-06-09 17:30:18 +0800

    Fix Api to SSL

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 Vue.directive('demo-widget', {
   bind: async (element, binding) => {
-    const res = await fetch('http://jdata.azurewebsites.net/api/files/FoodItem.json');
+    const res = await fetch('https://jdata.azurewebsites.net/api/files/FoodItem.json');
     const data = await res.json();
     element.innerHTML = data.map(({ imgUrl, name }) => {
       return `<div style="display:inline-block;width:150px"><span>${name}</span><img width="150px" height="150px" src="${imgUrl}" alt=""/></div>`
@@ -10,7 +10,7 @@ Vue.directive('demo-widget', {
   }
 })
 
-const dataUrl = 'http://jdata.azurewebsites.net/api/files/cmsdemo.json';
+const dataUrl = 'https://jdata.azurewebsites.net/api/files/cmsdemo.json';
 Vue.component('cms-subscriber', async (resolve, reject) => {
   const res = await fetch(dataUrl);
   const data = await res.json();
